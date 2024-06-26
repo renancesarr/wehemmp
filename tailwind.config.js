9a2a905b5c52908a7f3fcd48362ca057546843ea
Renan Cesar <renancesarr@users.noreply.github.com>
2024-06-26 05:31:16 -0300

    - Update package.json dependencies - Add anexo2.png to src/img - Add anexo3.png to src/img - Add anexo6.jpg to src/img - Modify src/index.html to include new images - Update Tailwind CSS configuration in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend:{
+      colors: {
+        greenButton: '#046307',
+        greenButton2: '#556B2F'
+      },
+      textShadow: {
+        sm: '1px 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '2px 2px 4px var(--tw-shadow-color)',
+        lg: '4px 4px 8px var(--tw-shadow-color)',
+        xl: '4px 4px 16px var(--tw-shadow-color)',
+      }
+    }
+    
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const newUtilities = {};
+      Object.keys(theme('textShadow')).forEach(key => {
+        const value = theme('textShadow')[key];
+        newUtilities[`.text-shadow-${key}`] = {
+          textShadow: value
+        };
+      });
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }
+  ]
 }
